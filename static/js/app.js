c9d9d26af38c3905648757090b20160ee0434211
Ekta Sharma <ekta.sharma3@gmail.com>
2020-03-06 18:14:50 -0600

    added missing semi colons in js code

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ const tableData = data;
 
 // finding unique values for each filter category (date, city, state, country and shape)
 const dateList = tableData.map(item => item.datetime)
-    .filter((value, index, self) => self.indexOf(value) === index);
+    .filter((value, index, self) => self.indexOf(value) === index).sort();
 const cityList = tableData.map(item => item.city)
     .filter((value, index, self) => self.indexOf(value) === index).sort();
 const stateList = tableData.map(item => item.state)
@@ -16,19 +16,19 @@ const shapeList = tableData.map(item => item.shape)
 // adding unique values to drop down list options for each filter category on the page (date, city, state, country and shape)
 dateList.forEach(item => {
     d3.select("#datetime_list").append("option").append("value").text(item)
-})
+});
 cityList.forEach(item => {
     d3.select("#city_list").append("option").append("value").text(item)
-})
+});
 stateList.forEach(item => {
     d3.select("#state_list").append("option").append("value").text(item)
-})
+});
 countryList.forEach(item => {
     d3.select("#country_list").append("option").append("value").text(item)
-})
+});
 shapeList.forEach(item => {
     d3.select("#shape_list").append("option").append("value").text(item)
-})
+});
 
 // constant decalartions for data table creation
 const table = d3.select("#ufo-table");
@@ -46,7 +46,7 @@ const handler = function() {
 
     tbody.html("");
 
-    let filterDate = inputDate.property("value").trim()
+    let filterDate = inputDate.property("value").trim();
     let filterCity = inputCity.property("value").toLowerCase().trim();
     let filterState = inputState.property("value").toLowerCase().trim();
     let filterCountry = inputCountry.property("value").toLowerCase().trim();
@@ -68,7 +68,8 @@ const handler = function() {
     };
     if (filterShape !== "") {
         filteredData = filteredData.filter(dataRow => dataRow.shape === filterShape)
-    };
+    } ;
+    // else filteredData = tableData;
 
     filteredData.forEach(dataRow => {
         let row = tbody.append("tr");
@@ -87,7 +88,7 @@ const clearTable = function() {
     // tbody.html("");
     // d3.select("#datetime").property("value") = "";
     // test = "";
-}
+};
 
 // calling the function to add data table on the web page when the filter button is clicked or enter key is pressed
 inputDate.on("change", handler);
@@ -96,4 +97,4 @@ inputState.on("change", handler);
 inputCountry.on("change", handler);
 inputShape.on("change", handler);
 filterButton.on("click", handler);
-resetButton.on("click", clearTable)
+resetButton.on("click", clearTable);
